f7041f63cdebe5ccc3f11fdae58a712522b48b5e
Nicholas Robbins <nl7robbins@icloud.com>
2020-04-26 15:03:02 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,11 +1,39 @@
+#1
 def sort_array_asc(array)
 
 end
 
+#2
 def sort_array_desc(array)
 
 end
 
+#3
 def sort_array_char_count(array)
+
+end
+
+#4
+def swap_elements(array)
+
+end
+
+#5
+def reverse_array(array)
+
+end
+
+#6
+def kesha_maker(array)
+
+end
+
+#7
+def find_a(array)
+
+end
+
+#8
+def sum_array(array)
 
 end
